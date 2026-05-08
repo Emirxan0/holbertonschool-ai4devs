8d055dfec95297384332cffd978b14bff223ede1
Emirxan0 <mirxanhmdli@gmail.com>
2026-05-08 14:23:19 +0400

    Create bug4_fixed.java

---
+++ prompting_debug_assistant/bug_snippets/bug_fixes/bug4_fixed.java
@@ -0,0 +1,17 @@
+public class InventoryManager {
+    public static void main(String[] args) {
+        String[] products = {"Laptop", "Mouse", "Keyboard"};
+        int[] stock = {10, 0, 5};
+
+        System.out.println("Anbar yoxlanışı başlayır...");
+
+        for (int i = 0; i < products.length; i++) {
+            if (stock[i] == 0) { 
+                System.out.println("XƏTA: " + products[i] + " tükənib!");
+            }
+
+            String detail = products[i].toLowerCase();
+            System.out.println("Məhsul: " + detail);
+        }
+    }
+}

3493b04df29433ebfb5539e30efcf2d338113947
Emirxan0 <mirxanhmdli@gmail.com>
2026-05-08 12:55:24 +0400

    Update bug1.py

--- prompting_debug_assistant/bug_snippets/bug1.py
+++ prompting_debug_assistant/bug_snippets/bug1.py
@@ -1,11 +1,11 @@
 def analyze_sensor_data(readings):
     valid_readings = []
     for i in range(len(readings)):
-        if readings[i] < 0: 
+        if readings[i] < 0:
             valid_readings.append(readings[i])
             
     total = 0
-    for j in range(0, len(valid_readings) - 1): 
+    for j in range(0, len(valid_readings) - 1):
         total += valid_readings[j]
         
     average = total / len(valid_readings)

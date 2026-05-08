e93214e4f2e12ed344d590e35cf14b089a1a556b
Emirxan0 <mirxanhmdli@gmail.com>
2026-05-08 12:55:49 +0400

    Update bug4.java

--- prompting_debug_assistant/bug_snippets/bug4.java
+++ prompting_debug_assistant/bug_snippets/bug4.java
@@ -2,19 +2,15 @@ public class InventoryManager {
     public static void main(String[] args) {
         String[] products = {"Laptop", "Mouse", "Keyboard"};
         int[] stock = {10, 0, 5};
-
+        
         System.out.println("Anbar yoxlanışı başlayır...");
-
+        
         for (int i = 0; i <= products.length; i++) {
-            
-            if (stock[i] == 0); { 
+            if (stock[i] == 0); {
                 System.out.println("XƏTA: " + products[i] + " tükənib!");
             }
-
             String detail = products[i].toLowerCase();
-            System.out.println("Məhsul emal edildi: " + detail);
+            System.out.println("Məhsul: " + detail);
         }
-        
-        System.out.println("Yoxlama bitdi.");
     }
 }

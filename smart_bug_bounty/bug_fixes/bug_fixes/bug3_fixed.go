ce13d0287d6bd0cc3d6cf8521bfdae495c9038c9
root <mirxanhmdli@gmail.com>
2026-05-14 21:52:25 +0000

    Remove nested bug_fixes directory

--- smart_bug_bounty/bug_fixes/bug_fixes/bug3_fixed.go
+++
@@ -1,9 +0,0 @@
-package main
-import ("fmt"; "sync")
-func main() {
-    var wg sync.WaitGroup
-    wg.Add(1)
-    go func() { defer wg.Done(); fmt.Println("Processing data...") }()
-    wg.Wait()
-    fmt.Println("Main function finished")
-}
